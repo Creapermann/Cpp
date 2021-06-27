fc945ecb1369f4ab980ae416590afd8890e20ec5
Creapermann <creapermann37@gmail.com>
2021-06-27 17:33:47 +0200

    init reset

---
+++ 28.12.2020_SnakeTheGame/build/CMakeFiles/Snake.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Creap/Data/Programming/C++/Codefiles/28.12.2020_SnakeTheGame/src/main.cpp" "/mnt/c/Users/Creap/Data/Programming/C++/Codefiles/28.12.2020_SnakeTheGame/build/CMakeFiles/Snake.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../extern/conio"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/c/Users/Creap/Data/Programming/C++/Codefiles/28.12.2020_SnakeTheGame/build/extern/conio/CMakeFiles/conio.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 28.12.2020_SnakeTheGame/build/CMakeFiles/SnakeTheGame.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Creap/Data/Programming/C++/Codefiles/28.12.2020_SnakeTheGame/main.cpp" "/mnt/c/Users/Creap/Data/Programming/C++/Codefiles/28.12.2020_SnakeTheGame/build/CMakeFiles/SnakeTheGame.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
