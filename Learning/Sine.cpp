dc7eab2c4e1669c99850340396944e9add046a52
Creapermann <creapermann37@gmail.com>
2021-08-16 16:59:05 +0200

    Sine.cpp added

---
+++ Learning/Sine.cpp
@@ -0,0 +1,42 @@
+﻿#include <vector>
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include <chrono>
+#include <string>
+#include <fstream>
+
+
+
+double sin(double x, int n)
+{
+    static double p{ 1 }, fCount{ 1 };
+
+    if (n == 1)
+        return x;
+
+    double res = sin(x, n - 1);
+
+    if (n == 2)         // First after base-case
+    {
+        p = x * x * x;
+        fCount = 3;
+    }
+    else
+    {
+        p = p * x * x;
+        fCount += 2;
+    }
+
+    int f{1};
+    for (int i = 1; i < fCount + 1; ++i)
+        f *= i;
+
+    return (n % 2 == 0) ? (res - p / f) : (res + p / f);
+}
+
+int main()
+{
+    std::cout << sin(10, 5);
+    std::cin.get();
+}
